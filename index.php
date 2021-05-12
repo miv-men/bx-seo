7e83009918f18610d1f0f0e425de691e8db0a51e
Ilia Malashko <35610631+miv-men@users.noreply.github.com>
2021-05-12 12:52:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,155 +1,28 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml,0/loose.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link href="css/style.css" rel="stylesheet" type="text/css" />
-<link href="css/reset.css" rel="stylesheet" type="text/css" />
-<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-    <script type="text/javascript" src="js/jquery-1.8.2.min.js"></script> 
-    <script type="text/javascript" src="js/jcarousellite_1.0.1.js"></script> 
-    <script type="text/javascript" src="js/jquery-ui-1.10.4.custom.js"></script> 
-    <script type="text/javascript" src="js/jquery.cookie.min.js"></script>
-    <script type="text/javascript" src="js/script.js"></script>
-<title>EDOK - Доставка еды</title>
-</head>
-<body>
-<div class="block-header">
-    <a href="index.php"><img src="img/logo.jpg"/></a>
-    <div class="head-contact">
-       <i class="gray fa fa-mobile"></i><p class="green-dark">&nbsp;&nbsp;&nbsp;&nbsp;8 (950) 575-44-31</p>
-       <p class="gray">ПН-ВС с 8:00 до 20:00</p>
-    </div>
-    <div class="head-menu">
-        <ul>
-        <li><a href="index.php"><i class="green fa fa-wpforms"></i><p>Меню</p></a></li>
-        <li><a href="sale.php"><i class="gray fa fa-birthday-cake"></i><p>Скидки</p></a></li>
-        <li><a href="delivery.php"><i class="gray fa fa-truck"></i><p>Доставка и оплата</p></a></li>
-        </ul>
-    </div>
-    <div class="head-cart">
-        <img src="img/basket.png" />
-        <p>Корзина пуста</p>
-    </div>
-</div>
-<div class="block-main">
-    <div class="block-chooce">
-    <img src="img/star.png" />
-        <h2>Категории</h2>
-        <ul class="category">
-            <li>Блины</li>
-            <li>Чапати</li>
-            <li>Сладости</li>
-            <li>Пирожки</li>
-        </ul>
-    <div class="advent">
-        <img src="img/SuperPizza.jpg"/>
-    </div>
-    </div>
-    <div class="block-menu">
-        <h1>Блины</h1>
-        <ul class="goods">
-            <li>
-            <img src="img/goods/d5.jpg"/>
-            <div class="comp">
-                <h6>Состав:</h6>
-                <p>Мука, молоко, соль, сахар, сода, масло слив., вода</p>
-            </div>
-            <h3>Блины обычные с маслом</h3>
-            <p  class="gray">200 гр. (10 шт.)</p>
-            <h1>150 <span class="gray">руб.</span></h1>
-            <input type="submit" value="Заказать"/>
-            </li>
-            <li>
-            <img src="img/goods/d4.jpg"/>
-            <div class="comp">
-                <h6>Состав:</h6>
-                <p>Мука, молоко, соль, сахар, сода, масло слив., воды, клубника, шоколад "Аленка", крахмал, изюм, грецкий орех, сметана</p>
-            </div>
-            <h3>Блин шоколадный</h3>
-            <p class="gray">200 гр. (10 шт.)</p>
-            <h1>150 <span class="gray">руб.</span></h1>
-            <input type="submit" value="Заказать"/>
-            </li>
-            <li>
-            <img src="img/goods/d5.jpg"/>
-            <div class="comp">
-                <h6>Состав:</h6>
-                <p>Мука, молоко, соль, сахар, сода, масло слив., воды, изюм, грецкий орех, сметана, творог</p>
-            </div>
-            <h3>Блин с творогом</h3>
-            <p class="gray">200 гр. (10 шт.)</p>
-            <h1>150 <span class="gray">руб.</span></h1>
-            <input type="submit" value="Заказать"/>
-            </li>
-            <li>
-            <img src="img/goods/d2.jpg"/>
-            <div class="comp">
-                <h6>Состав:</h6>
-                <p>Мука, молоко, соль, сахар, сода, масло слив., воды, клубника, шоколад "Аленка", крахмал, изюм, грецкий орех, сметана</p>
-            </div>
-            <h3>Блин с соусом</h3>
-            <p class="gray">200 гр. (10 шт.)</p>
-            <h1>150 <span class="gray">руб.</span></h1>
-            <input type="submit" value="Заказать"/>
-            </li>
-            <li>
-            <img src="img/goods/ddd.jpg"/>
-            <div class="comp">
-                <h6>Состав:</h6>
-                <p>Мука, молоко, соль, сахар, сода, масло слив., вода</p>
-            </div>
-            <h3>Блин "ягодный"</h3>
-            <p class="gray">200 гр. (10 шт.)</p>
-            <h1>150 <span class="gray">руб.</span></h1>
-            <input type="submit" value="Заказать"/>
-            </li>
-            <li>
-            <img src="img/goods/ddd.jpg"/>
-            <div class="comp">
-                <h6>Состав:</h6>
-                <p>Мука, молоко, соль, сахар, сода, масло слив., вода</p>
-            </div>
-            <h3>Блин "ягодный"</h3>
-            <p class="gray">200 гр. (10 шт.)</p>
-            <h1>150 <span class="gray">руб.</span></h1>
-            <input type="submit" value="Заказать"/>
-            </li>
-            <li>
-            <img src="img/goods/ddd.jpg"/>
-            <div class="comp">
-                <h6>Состав:</h6>
-                <p>Мука, молоко, соль, сахар, сода, масло слив., вода</p>
-            </div>
-            <h3>Блин "ягодный"</h3>
-            <p class="gray">200 гр. (10 шт.)</p>
-            <h1>150 <span class="gray">руб.</span></h1>
-            <input type="submit" value="Заказать"/>
-            </li>
-        </ul>
-    </div>
+<?php
+/*** Р’ РєР°С‚Р°Р»РѕРіРµ: ***/
+$seoData = new miv\SeoText($_SERVER["SERVER_NAME"]);
 
-</div>
-<div class="footer">
-    <div class="foot-logo">
-       <img src="img/logo-w.png"/>
-       <div class="brend"><i class="fa fa-copyright"></i><p> EDOK.RU - 2016</p></div>
+$pageNumber = intVal($_REQUEST['PAGEN_2']) ? intVal($_REQUEST['PAGEN_2']) : false;
+$arSeoParams = array(
+    'section_id' => $arResult["VARIABLES"]["SECTION_ID"],
+    'page_number' => $pageNumber
+);
+
+$seoData->initData('section', $arSeoParams); // РџРѕР»СѓС‡Р°РµРј СЃРµРѕ С‚РµРєСЃС‚С‹ РІ Р·Р°РІРёСЃРёРјРѕСЃС‚Рё РѕС‚ СЂР°Р·РґРµР»Р° Рё в„– СЃС‚СЂР°РЅРёС†С‹
+
+/* Р—Р°РґР°РµРј РјРµС‚Р° С‚РµРіРё */
+$APPLICATION->SetPageProperty("title", $seoData->title);
+$APPLICATION->SetPageProperty("description", $seoData->description);
+
+echo '<h1>' . $seoData->h1 . '</h1>'; // Р—Р°РіРѕР»РѕРІРѕРє СЃС‚СЂР°РЅРёС†С‹
+
+/* РћРїРёСЃР°РЅРёРµ СЂР°Р·РґРµР»Р° (РІС‹РІРѕРґРёРј С‚РѕР»СЊРєРѕ РґР»СЏ РїРµСЂРІРѕР№ СЃС‚СЂР°РЅРёС†С‹) */
+if ( strlen($seoData->top_description['TEXT']) && strpos($_SERVER['REQUEST_URI'], 'PAGEN_') === false): ?>
+    <div class="section__top-description">
+        <?php if ($seoData->top_description['TYPE'] == 'HTML'): ?>
+            <?=html_entity_decode($seoData->top_description['TEXT'])?>
+        <?php else: ?>
+            <?=$seoData->top_description['TEXT']?>
+        <?php endif; ?>
     </div>
-    <div class="cont-f">
-        <i class="fa fa-mobile"></i><p>&nbsp;&nbsp;&nbsp;&nbsp;8 (950) 575-44-31</p>
-        <p>ПН-ВС с 8:00 до 20:00</p>
-    </div>
-    <div class="subs">
-        <i class="fa fa-envelope-o "></i><h2>Подписка</h2>
-        <p>Рассылка проходящих акций и обновлений в меню.</p>
-        <form>
-        <input class="subs-in" type="text" />
-        <input class="subs-submit" type="submit" value="&rarr;"/>
-        </form>
-    </div>
-</div>
-
-
-
-
-</body>
-</html>
+<?php endif; ?>
